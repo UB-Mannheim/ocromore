160503e2f2931cf794599db2f2499f7e73192f1f
johannes <johannesst@gmx.de>
2017-12-12 18:51:09 +0100

    Added sorting sets.

--- bbox_comparator.py
+++ bbox_comparator.py
@@ -508,6 +508,18 @@ class OCRcomparison:
     def add_set(self,set_to_add):
         self.ocr_sets.append(set_to_add)
 
+    def sort_set(self):
+        """
+        Sort the ocr_sets by y_mean values
+        :return:
+        """
+        def take_y_mean(my_set):
+            ym = my_set.y_mean
+            return ym
+
+        sorted_sets = sorted(self.ocr_sets, key=take_y_mean, reverse=False)
+        self.ocr_sets = sorted_sets
+
     def print_sets(self, diff_only= False):
         for current_set in self.ocr_sets:
             current_set.print_me(diff_only)
@@ -553,4 +565,5 @@ def compare_lists_new(ocr_lists):
 
 
 ocr_comparison = compare_lists_new(base_ocr_lists)
-ocr_comparison.print_sets(True)
+ocr_comparison.sort_set()
+ocr_comparison.print_sets(False)
