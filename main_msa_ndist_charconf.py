e0ae33d893a5ff7c2f90572efd1c2bf676c26634
johannes <johannesst@gmx.de>
2018-07-05 11:26:56 +0200

    corrected postcorrection of 'I1' to 'II' etc. Works now, sligt improvement ~12 errors in manyyears

--- main_msa_ndist_charconf.py
+++ main_msa_ndist_charconf.py
@@ -46,6 +46,9 @@ for db in dh.db:
     temp = os.path.splitext(db)[0]
     db_keyname = os.path.basename(temp)  # this returns just the filename (wildlife)
 
+    #if  "1969" not in db_keyname:
+    #    continue
+
     files_gt = filestructs_gt[db_keyname]
     for file in files:
         count += 1
@@ -54,7 +57,7 @@ for db in dh.db:
         dbpath = 'sqlite:////' + db
         print("Parsing table: ", table, "in database: ", dbpath)
 
-        #if "264" not in table: continue
+        #if "624" not in table: continue
 
 
         table_ctr += 1
--- n_dist_keying/text_corrector.py
+++ n_dist_keying/text_corrector.py
@@ -45,35 +45,42 @@ class TextCorrector(object):
     @staticmethod
     def correct_roman_ii(line_text):
         """
-        todo: this correction has to be verified for false positives 
         Corrects occurrences of roman II letters which get confused with I1 or 11 sometimes
 
         Example:
         Groundtruth: 1948/II/49 bis 1954: je 0 %
         line_text  : 1948/11/49 bis 1954: je 0%
+        or
+        Groundtruth: II/1955/42 text text
+        line_text:   1I/1955/42 text text
 
         :param line_text:
         :return:
         """
 
-        # check if the date pattern occurs
-        # \d: Matches any decimal digit
-        # \w: Matches any alphanumeric character (alphabet or number)
 
-        rii_regex = re.compile('\d\d\d\d/\w\w/\d\d')
+
+        # check if the possibly erronous line occurs:
+        # if the first 2 or more characters are 1, or I, or l, or i
+        # and following 3 or more are decimals, and following 2 or more are decimals
+        # (seperated by slashes)
+        rii_regex = re.compile('[1Ili]{2,}\/\d{3,}/\d{2,}')
         rii_match = rii_regex.match(line_text)
 
-        # return the unchanged text, if the pattern doesn't occur
-        if rii_match is None:
-            return line_text
+        rii_regex2 = re.compile('\d{4,}\/[1Ili]{2}/\d{2,}')
+        rii_match2 = rii_regex2.match(line_text)
 
-        # if the pattern occurs, refactor line_text (todo: this is simple-mode replacement, make it better)
-        rii_string = rii_match.group()
-        rii_string = rii_string.replace('/11/', '/II/')
-        rii_string = rii_string.replace('/I1/', '/II/')
-        rii_string = rii_string.replace('/1I/', '/II/')
 
-        # substitute the corrected text in original string
-        new_line_text = rii_regex.sub(rii_string, line_text)
+        # return the changed text, if the pattern   occurs
+        if rii_match is not None:
+            subst_string = re.sub('[1Ili]{2,}\/', "II/", line_text)
+            # place the corrected roman two in line_text
+            return subst_string
+        elif rii_match2 is not None:
+            # todo verify this once with bp the case definitely exists but not in manyyears
+            subst_string = re.sub('[1Ili]{2}\/', "II/", line_text)
+            # place the corrected roman two in line_text
+            return subst_string
 
-        return new_line_text
+        # return unchanged if pattern doesn't occur
+        return line_text
