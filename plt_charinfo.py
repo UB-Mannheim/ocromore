47cb20a2f80782e1dc23766481b891fe4c6bf7d4
johannes <johannesst@gmx.de>
2019-02-20 12:01:55 +0100

    refactored configuration files (untested)

--- plt_charinfo.py
+++ plt_charinfo.py
@@ -8,11 +8,7 @@ from pathlib import Path
 from configuration.configuration_handler import ConfigurationHandler
 from akf_corelib.database_handler import DatabaseHandler
 
-CODED_CONFIGURATION_PATH = "./configuration/to_db_reader/config_read_akftest_jk.conf"
-PRINT_SUSPICIOUSLINES = False
-CLEAN_ABBYY = False
-VERBOSE = False
-VERBOSEPATH = "/media/sf_ShareVB/AFKII/verbose/"
+CODED_CONFIGURATION_PATH = "./configuration/to_db_reader/config_read_akf_usb.conf"
 
 config_handler = ConfigurationHandler(first_init=True, fill_unkown_args=True, coded_configuration_paths=[CODED_CONFIGURATION_PATH])
 config = config_handler.get_config()
@@ -20,26 +16,30 @@ config = config_handler.get_config()
 # Read hocr and create sql-db
 dbdir = str(Path(config.DBDIR_READER).absolute())
 dh = DatabaseHandler(dbdir=dbdir)
-dh.set_dirpos(tablename_pos=config.TABLENAME_POS,ocr_profile_pos=config.OCR_PROFILE_POS,\
-              ocr_pos=config.OCR_POS,dbname_pos=config.DBPATH_POS)
+dh.set_dirpos(tablename_pos=config.TABLENAME_POS, ocr_profile_pos=config.OCR_PROFILE_POS,\
+              ocr_pos=config.OCR_POS, dbname_pos=config.DBPATH_POS)
 
 dh.fetch_files(config.INPUT_FILEGLOB, config.INPUT_FILETYPES)
-test = dh.get_files()
-#dh.update_db()
-#dh.work_with_object(dh.dburlscheme+dh.db[0],dh.tablefilter)
+# test = dh.get_files() # just a simple check if files were read
+
 
 if config.HOCR2SQL:
+    # Write the hocr files from output of the first step to sql-database
     report_conv = dh.parse_to_db(delete_and_create_dir=config.DELETE_AND_CREATE_DBDIR)
 
 dh.update_db()
 
 if config.PREPROCESSING:
-    #TODO: Add verbose to configfiles
-    report_prep = dh.preprocess_dbdata(force=True, PRINT_SUSPICIOUSLINES = PRINT_SUSPICIOUSLINES, CLEAN_ABBYY=CLEAN_ABBYY, VERBOSE=VERBOSE, VERBOSEPATH=VERBOSEPATH)
+    # do pre-alignment of data (lines in the files engine-wise, word-wise, ... )
+    report_prep = dh.preprocess_dbdata(force=True, PRINT_SUSPICIOUSLINES=config.PRINT_SUSPICIOUSLINES,
+                                       CLEAN_ABBYY=config.CLEAN_ABBYY, VERBOSE=config.VERBOSE,
+                                       VERBOSEPATH=config.VERBOSEPATH)
 
-#if config.WORKWITHOBJ:
-#    dh._work_with_object(dh.dburlscheme+dh.db[0],dh.tablefilter)
+if config.WORKWITHOBJ:
+    # test the data-integrity (did alignment steps work ...)
+    dh._work_with_object(dh.dburlscheme+dh.db[0], dh.tablefilter)
 
 # Plot DF (not working atm)
-#if config.PLOT:
-#    dh._plot_charinfo()
+if config.PLOT:
+    # deprecated function to plot results
+    dh._plot_charinfo()
